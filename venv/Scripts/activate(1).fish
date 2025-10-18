38ffa845d988a87cbce1c0e5686c51332ee43ac7
Shweta Katkar <114807467+katkarsshweta@users.noreply.github.com>
2025-10-18 22:37:17 +0530

    Add files via upload

--- venv/Scripts/activate(1).fish
+++ venv/Scripts/activate(1).fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\Shwet\Downloads\dieT\venv'
+set -gx VIRTUAL_ENV 'C:\Users\Shwet\Downloads\dieT\HealthWiseDiet\venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
